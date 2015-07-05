60bbac195a326b6aeff761d6a7f646d3b8ca80fc
Susie Lu <slu@netflix.com>
2015-07-05 12:40:06 -0700

    seperated color legend from size legend. Added in transitions

--- legend.js
+++ legend.js
@@ -1,92 +1,10 @@
+d3.legend = {};
 
-d3.svg.legend = function() {
-  //default settings
-  var scale = d3.scale.linear(),
-    shape = "rect", //options rect circle or line
-    shapeWidth = 15, //think about these
-    shapeHeight = 15, //think about these
-    shapeRadius = 10,
-    shapePadding = 2,
-    cells = [5],
-    labels = [],
-    attribute = "fill",
-    labelFormat = d3.format(".01f"),
-    labelOffset = 10,
-    orient = "vertical";
+function d3_mergeLabels(gen, labels) {
 
-  function legend(svg){
-
-    var type = scale.ticks ? linear() : scale.invertExtent ? quant() : ordinal();
-    type.labels = mergeLabels(type.labels);
-
-    var cell = svg.selectAll(".cell"),
-      cellEnter = cell.data(type.data).enter().insert("g", ".lgdCells").attr("class", "cell"),
-      shapeEnter = cellEnter.append(shape).attr("class", "swatch"),
-      shapes = cellEnter.select(shape);
-
-      //cellExit, needs to be added
-      //cellUpdate, needs to be added
-
-    //creates shape
-    if (shape === "rect"){
-        shapes.attr("height", shapeHeight).attr("width", shapeWidth);
-
-    } else if (shape === "circle") {
-        shapes.attr("r", shapeRadius).attr("cx", shapeRadius).attr("cy", 0);
-
-    } else if (shape === "line") {
-        shapes.attr("x1", 0).attr("x2", shapeWidth).attr("y1", 0).attr("y2", 0)
-    }
-
-    //adds text
-    cellEnter.append("text").attr("class", "label");
-    svg.selectAll("g.cell text").data(type.labels)
-      .text(function(d) { return d; });
-
-    // sets placement
-    var swatchEnter = cellEnter.select(shape),
-      textEnter = cellEnter.select("text"),
-      size = swatchEnter[0].map(function(d){ return d.getBBox(); });
-
-    if (orient === "vertical"){
-      cellEnter.attr("transform",
-        function(d,i) {
-          return "translate(0, " + (i * (size[i].height + shapePadding)) + ")";
-        });
-
-      textEnter.attr("transform",
-        function(d,i) {
-          return "translate(" + (size[i].width + labelOffset) + "," +
-            size[i].height * .75 + ")";
-        });
-
-    } else if (orient === "horizontal"){
-      cellEnter.attr("transform",
-        function(d,i) {
-          return "translate(" + (i * (size[i].width + shapePadding)) + ",0)";
-        });
-
-      textEnter.attr("transform",
-        function(d,i) {
-          return "translate(" + size[i].width/2 + "," + (size[i].height +
-              labelOffset + 5) + ")";
-        })
-        .style("text-anchor", "middle");
-    }
-
-    // sets color
-    if (attribute === "fill"){
-      swatchEnter.style("fill", type.fill);
-    } else if (attribute === "class"){
-      swatchEnter.attr("class", function(d){ return "swatch " + type.fill(d); });
-    } else if (attribute === "stroke"){
-      swatchEnter.style("stroke", type.fill);
-    }
-
-  }
-
-  function mergeLabels(gen) {
     if(labels.length === 0) return gen;
+
+    gen = (gen) ? gen : [];
 
     var i = labels.length;
     for (; i < gen.length; i++) {
@@ -95,117 +13,51 @@ d3.svg.legend = function() {
     return labels;
   }
 
-  function linear() {
-    var data = [];
+function d3_linearLegend(scale, cells, labelFormat) {
+  var data = [];
 
-    if (cells.length > 1){
-      data = cells;
+  if (cells.length > 1){
+    data = cells;
 
-    } else {
-      var domain = scale.domain(),
-      increment = (domain[1] - domain[0])/(cells - 1),
-      i = 0;
+  } else {
+    var domain = scale.domain(),
+    increment = (domain[1] - domain[0])/(cells - 1),
+    i = 0;
 
-      for (; i < cells; i++){
-        data.push(labelFormat(domain[0] + i*increment));
-      }
-
+    for (; i < cells; i++){
+      data.push(labelFormat(domain[0] + i*increment));
     }
-
-    return {data: data,
-            labels: data,
-            fill: function(d){ return scale(d); }};
   }
 
-  function quant() {
-    var labels = scale.range().map(function(d){
-      var invert = scale.invertExtent(d);
-      return labelFormat(invert[0]) + " to " + labelFormat(invert[1]);
-    });
-    return {data: scale.range(),
-            labels: labels,
-            fill: function(d){ return d; }};
+  return {data: data,
+          labels: data,
+          fill: function(d){ return scale(d); }};
+}
+
+function d3_quantLegend(scale, labelFormat) {
+  var labels = scale.range().map(function(d){
+    var invert = scale.invertExtent(d);
+    return labelFormat(invert[0]) + " to " + labelFormat(invert[1]);
+  });
+  return {data: scale.range(),
+          labels: labels,
+          fill: function(d){ return d; }};
+}
+
+function d3_ordinalLegend(scale) {
+  return {data: scale.domain(),
+          labels: scale.domain(),
+          fill: function(d){ return scale(d); }};
+}
+
+function d3_drawShapes(shape, shapes, shapeHeight, shapeWidth, shapeRadius) {
+  if (shape === "rect"){
+      shapes.attr("height", shapeHeight).attr("width", shapeWidth);
+
+  } else if (shape === "circle") {
+      shapes.attr("r", shapeRadius).attr("cx", shapeRadius).attr("cy", 0);
+
+  } else if (shape === "line") {
+      shapes.attr("x1", 0).attr("x2", shapeWidth).attr("y1", 0).attr("y2", 0);
   }
-
-  function ordinal() {
-    return {data: scale.domain(),
-            labels: scale.domain(),
-            fill: function(d){ return scale(d); }};
-  }
-
-  //updating settings
-  legend.scale = function(_) {
-    if (!arguments.length) return legend;
-    scale = _;
-    return legend;
-  };
-
-  legend.cells = function(_) {
-    if (!arguments.length) return legend;
-    if (_.length > 1 || _ >= 2 ){
-      cells = _;
-    }
-    return legend;
-  };
-
-  legend.shape = function(_) {
-    if (!arguments.length) return legend;
-    shape = _;
-    return legend;
-  };
-
-  legend.shapeWidth = function(_) {
-    if (!arguments.length) return legend;
-    shapeWidth = +_;
-    return legend;
-  };
-
-  legend.shapeHeight = function(_) {
-    if (!arguments.length) return legend;
-    shapeHeight = +_;
-    return legend;
-  };
-
-  legend.shapePadding = function(_) {
-    if (!arguments.length) return legend;
-    shapePadding = +_;
-    return legend;
-  };
-
-  legend.labels = function(_) {
-    if (!arguments.length) return legend;
-    labels = _;
-    return legend;
-  };
-
-  legend.labelFormat = function(_) {
-    if (!arguments.length) return legend;
-    labelFormat = _;
-    return legend;
-  };
-
-  legend.labelOffset = function(_) {
-    if (!arguments.length) return legend;
-    labelOffset = +_;
-    return legend;
-  };
-
-  legend.attribute = function(_) {
-    if (!arguments.length) return legend;
-    if (_ == "fill" || _ == "class" || _ == "stroke"){
-      attribute = _;
-    }
-    return legend;
-  };
-
-  legend.orient = function(_){
-    if (!arguments.length) return legend;
-    _ = _.toLowerCase();
-    if (_ == "horizontal" || _ == "vertical") {
-      orient = _;
-    }
-    return legend;
-  }
-
-  return legend;
 }
---
+++ size.js
@@ -0,0 +1,151 @@
+
+d3.legend.size = function(){
+
+  var scale = d3.scale.linear(),
+    shape = "rect",
+    shapeWidth = 15,
+    shapeHeight = 15,
+    shapeRadius = 10,
+    shapePadding = 2,
+    cells = [5],
+    labels = [],
+    useClass = false,
+    labelFormat = d3.format(".01f"),
+    labelOffset = 10,
+    orient = "vertical";
+
+
+    function legend(svg){
+
+      var type = scale.ticks ?
+          d3_linearLegend(scale, cells, labelFormat) : scale.invertExtent ?
+          d3_quantLegend(scale, labelFormat) : d3_ordinalLegend(scale);
+      type.labels = d3_mergeLabels(type.labels, labels);
+
+      var cell = svg.selectAll(".cell").data(type.data),
+        cellEnter = cell.enter().append("g", ".cell").attr("class", "cell").style("opacity", 1e-6);
+        shapeEnter = cell.append(shape).attr("class", "swatch"),
+        shapes = cell.select(shape);
+
+      console.log(type.data)
+
+      //remove old shapes
+      cell.exit().transition().style("opacity", 0).remove();
+
+      //creates shape
+      d3_drawShapes(shape, shapes, shapeHeight, shapeWidth, shapeRadius);
+
+      //adds text
+      cellEnter.append("text").attr("class", "label");
+      svg.selectAll("g.cell text").data(type.labels)
+        .text(function(d) { return d; });
+
+      // sets placement
+      var  text = cellEnter.select("text"),
+        shapeSize = shapes[0].map(
+          function(d, i){
+            return d.getBBox();
+        });
+
+      //positions cells
+      if (orient === "vertical"){
+        cellEnter.attr("transform",
+          function(d,i) {
+            return "translate(0, " + (i * (shapeSize[i].height + shapePadding)) + ")";
+          })
+         .transition().style("opacity", 1);
+
+        text.attr("transform",
+          function(d,i) {
+            return "translate(" + (shapeSize[i].width + labelOffset) + "," +
+              shapeSize[i].height * .75 + ")";
+          });
+
+      } else if (orient === "horizontal"){
+        cellEnter.attr("transform",
+          function(d,i) {
+            return "translate(" + (i * (shapeSize[i].width + shapePadding)) + ",0)";
+          })
+         .transition().style("opacity", 1);
+
+        text.attr("transform",
+          function(d,i) {
+            return "translate(" + shapeSize[i].width/2 + "," + (shapeSize[i].height +
+                labelOffset + 5) + ")";
+          })
+          .style("text-anchor", "middle");
+      }
+    }
+
+
+
+  legend.scale = function(_) {
+    if (!arguments.length) return legend;
+    scale = _;
+    return legend;
+  };
+
+  legend.cells = function(_) {
+    if (!arguments.length) return legend;
+    if (_.length > 1 || _ >= 2 ){
+      cells = _;
+    }
+    return legend;
+  };
+
+  legend.shape = function(_) {
+    if (!arguments.length) return legend;
+    shape = _;
+    return legend;
+  };
+
+  legend.shapeWidth = function(_) {
+    if (!arguments.length) return legend;
+    shapeWidth = +_;
+    return legend;
+  };
+
+  legend.shapeHeight = function(_) {
+    if (!arguments.length) return legend;
+    shapeHeight = +_;
+    return legend;
+  };
+
+  legend.shapePadding = function(_) {
+    if (!arguments.length) return legend;
+    shapePadding = +_;
+    return legend;
+  };
+
+  legend.labels = function(_) {
+    if (!arguments.length) return legend;
+    labels = _;
+    return legend;
+  };
+
+  legend.labelFormat = function(_) {
+    if (!arguments.length) return legend;
+    labelFormat = _;
+    return legend;
+  };
+
+  legend.labelOffset = function(_) {
+    if (!arguments.length) return legend;
+    labelOffset = +_;
+    return legend;
+  };
+
+
+  legend.orient = function(_){
+    if (!arguments.length) return legend;
+    _ = _.toLowerCase();
+    if (_ == "horizontal" || _ == "vertical") {
+      orient = _;
+    }
+    return legend;
+  };
+
+  return legend;
+
+};
+
