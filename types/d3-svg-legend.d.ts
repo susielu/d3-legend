e0bd689381abdcb24d3aa10cd40478c0ed041fc4
Jorge Yagüe París <jorge.yague@treelogic.com>
2017-03-08 16:31:31 +0100

    Add initial TS typings

---
+++ types/d3-svg-legend.d.ts
@@ -0,0 +1,10 @@
+export function legendColor(...args: any[]): any;
+
+export function legendSize(...args: any[]): any;
+
+export function legendSymbol(...args: any[]): any;
+
+export namespace legendHelpers {
+    function thresholdLabels(_ref: any): any;
+
+}
