9e34682492dcf95119abd3656183cf43b978cf98
Clement Procureur <clement@sgrx04.afp.local>
2017-11-13 17:20:13 +0100

    init and install

--- indexRollupNext.js
+++ indexRollupNext.js
@@ -320,20 +320,18 @@ function color() {
 
     //positions cells and text
     if (orient === "vertical") {
-      (function () {
-        var cellSize = textSize.map(function (d, i) {
-          return Math.max(d.height, shapeSize[i].height);
-        });
+      var cellSize = textSize.map(function (d, i) {
+        return Math.max(d.height, shapeSize[i].height);
+      });
 
-        cellTrans = function cellTrans(d, i) {
-          var height = sum(cellSize.slice(0, i));
-          return 'translate(0, ' + (height + i * shapePadding) + ')';
-        };
+      cellTrans = function cellTrans(d, i) {
+        var height = sum(cellSize.slice(0, i));
+        return 'translate(0, ' + (height + i * shapePadding) + ')';
+      };
 
-        textTrans = function textTrans(d, i) {
-          return 'translate( ' + (shapeSize[i].width + shapeSize[i].x + labelOffset) + ', ' + (shapeSize[i].y + shapeSize[i].height / 2 + 5) + ')';
-        };
-      })();
+      textTrans = function textTrans(d, i) {
+        return 'translate( ' + (shapeSize[i].width + shapeSize[i].x + labelOffset) + ', ' + (shapeSize[i].y + shapeSize[i].height / 2 + 5) + ')';
+      };
     } else if (orient === "horizontal") {
       cellTrans = function cellTrans(d, i) {
         return 'translate(' + i * (shapeSize[i].width + shapePadding) + ',0)';
@@ -582,21 +580,19 @@ function size() {
 
     //positions cells and text
     if (orient === "vertical") {
-      (function () {
-        var cellSize = textSize.map(function (d, i) {
-          return Math.max(d.height, shapeSize[i].height);
-        });
-        var y = shape == "circle" || shape == "line" ? shapeSize[0].height / 2 : 0;
-        cellTrans = function cellTrans(d, i) {
-          var height = sum(cellSize.slice(0, i));
+      var cellSize = textSize.map(function (d, i) {
+        return Math.max(d.height, shapeSize[i].height);
+      });
+      var y = shape == "circle" || shape == "line" ? shapeSize[0].height / 2 : 0;
+      cellTrans = function cellTrans(d, i) {
+        var height = sum(cellSize.slice(0, i));
 
-          return 'translate(0, ' + (y + height + i * shapePadding) + ')';
-        };
+        return 'translate(0, ' + (y + height + i * shapePadding) + ')';
+      };
 
-        textTrans = function textTrans(d, i) {
-          return 'translate( ' + (maxW + labelOffset) + ',\n          ' + (shapeSize[i].y + shapeSize[i].height / 2 + 5) + ')';
-        };
-      })();
+      textTrans = function textTrans(d, i) {
+        return 'translate( ' + (maxW + labelOffset) + ',\n          ' + (shapeSize[i].y + shapeSize[i].height / 2 + 5) + ')';
+      };
     } else if (orient === "horizontal") {
       cellTrans = function cellTrans(d, i) {
         var width = sum(shapeSize.slice(0, i), function (d) {
@@ -811,19 +807,17 @@ function symbol() {
 
     //positions cells and text
     if (orient === "vertical") {
-      (function () {
-        var cellSize = textSize.map(function (d, i) {
-          return Math.max(maxH, d.height);
-        });
+      var cellSize = textSize.map(function (d, i) {
+        return Math.max(maxH, d.height);
+      });
 
-        cellTrans = function cellTrans(d, i) {
-          var height = sum(cellSize.slice(0, i));
-          return 'translate(0, ' + (height + i * shapePadding) + ' )';
-        };
-        textTrans = function textTrans(d, i) {
-          return 'translate( ' + (maxW + labelOffset) + ',\n              ' + (shapeSize[i].y + shapeSize[i].height / 2 + 5) + ')';
-        };
-      })();
+      cellTrans = function cellTrans(d, i) {
+        var height = sum(cellSize.slice(0, i));
+        return 'translate(0, ' + (height + i * shapePadding) + ' )';
+      };
+      textTrans = function textTrans(d, i) {
+        return 'translate( ' + (maxW + labelOffset) + ',\n              ' + (shapeSize[i].y + shapeSize[i].height / 2 + 5) + ')';
+      };
     } else if (orient === "horizontal") {
       cellTrans = function cellTrans(d, i) {
         return 'translate( ' + i * (maxW + shapePadding) + ',0)';
